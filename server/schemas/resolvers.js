ca701104cab8a6bbd9c766d06d4d16e733e19a22
Killa J <joshuaalexandercross@gmail.com>
2021-07-01 22:35:35 -0500

    added first query and ran GraphQL playground

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,9 @@
+const { User, Thought } = require('../models');
+
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello world!';
+        thoughts: async () => {
+            return Thought.find().sort({ createAt: -1 });
         }
     }
 };
