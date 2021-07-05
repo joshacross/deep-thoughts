fd40053248ce078687596a3d995f76868f5daf07
Killa J <joshuaalexandercross@gmail.com>
2021-07-05 18:38:22 -0500

    added authentication and context middleware to mutations

--- server/server.js
+++ server/server.js
@@ -4,6 +4,9 @@ const { ApolloServer } = require('apollo-server-express');
 
 //import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
+//import authentication middleware
+const { authMiddleware } = require('./utils/auth')
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -11,7 +14,8 @@ const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 //integrate Apollo server with Express as middleware
